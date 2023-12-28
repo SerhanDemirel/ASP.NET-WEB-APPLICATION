bf5bb577a86587a6688442ca6dac1d02c59ace75
Serhan Demirel <102233776+SerhanDemirel@users.noreply.github.com>
2023-12-28 15:08:37 +0300

    Add project files.

---
+++ MvcProje/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda ()
+        {
+            return View();
+        }
+        public ActionResult Takim()
+        {
+            return View();
+        }
+        public ActionResult Servis ()
+        {
+            return View();
+        }
+        public ActionResult Portfoy()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
